a27508b40e4601052eae9a611b305c41f6be8e27
iva28 <iva.hurt574@gmail.com>
2023-05-16 13:14:45 +0200

    Inicijalni commit za server stranu aplikacije ordinacije

---
+++ OrdinacijaServer/src/main/java/rs/ac/bg/fon/ai/nprog/OrdinacijaServer/App.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.ai.nprog.OrdinacijaServer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
