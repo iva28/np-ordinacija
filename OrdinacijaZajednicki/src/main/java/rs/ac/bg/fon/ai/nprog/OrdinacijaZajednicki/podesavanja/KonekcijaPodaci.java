c07c2ef1f56a52e94320437595d33c1f1d9645bd
iva28 <iva.hurt574@gmail.com>
2023-05-23 14:29:30 +0200

    Dodata dokumentacija u fajl KonekcijaPodaci u zajednickom projektu

--- OrdinacijaZajednicki/src/main/java/rs/ac/bg/fon/ai/nprog/OrdinacijaZajednicki/podesavanja/KonekcijaPodaci.java
+++ OrdinacijaZajednicki/src/main/java/rs/ac/bg/fon/ai/nprog/OrdinacijaZajednicki/podesavanja/KonekcijaPodaci.java
@@ -4,11 +4,30 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.Properties;
-
+/**
+ * Predstavlja informacije za povezivanje na server i za upravljanje njima
+ * 
+ * Iscitava podatke iz konekcija.properties fajla, koji sadrzi podatke o portu i adresi na kojem je podignut server
+ * 
+ * @author Iva Stanisic
+ *
+ */
 public class KonekcijaPodaci {
+	
+	/**
+	 * Instanca klase KonekcijaPodaci implementirana kao singleton
+	 * 
+	 * Razlog ovakve implementacije je da bi svi objekti koji pozivaju ovu klasu, pozivali isti objekat
+	 */
 	private static KonekcijaPodaci instanca;
+	/**
+	 * Objekat za pristup properties fajlu
+	 */
 	private Properties props;
 
+	/**
+	 * Privatni konstruktor koji se poziva samo jednom, samo prvi put
+	 */
 	private KonekcijaPodaci() {
 		try {
 			props = new Properties();
@@ -18,6 +37,13 @@ public class KonekcijaPodaci {
 		}
 	}
 
+	/**
+	 * Vraca instancu na objekat klase KonekcijaPodaci
+	 * 
+	 * Ukoliko pre toga objekat nije bio instanciran kreira novu instancu u suprotnom vraca postojecu
+	 * 
+	 * @return instanca klase KonekcijaPodaci
+	 */
 	public static KonekcijaPodaci vratiInstancu() {
 		if (instanca == null) {
 			instanca = new KonekcijaPodaci();
@@ -25,10 +51,22 @@ public class KonekcijaPodaci {
 		return instanca;
 	}
 
+	/**
+	 * Vraca property iz properties fajla na osnovu prosledjenog kljuca
+	 * 
+	 * @param kljuc vrednost na osnovu koje se pretrazuje properties fajl
+	 * @return String vrednost odgovarajuceg kljuca
+	 */
 	public String vratiProperti(String kljuc) {
 		return props.getProperty(kljuc);
 	}
 
+	/**
+	 * Postavlja novu adresu na kojoj server ceka klijenta
+	 * 
+	 * @param adresa serverskog soketa
+	 * @throws Exception ako dodje do greske pri upisu adrese u fajl
+	 */
 	public void postaviAdresu(String adresa) {
 		try {
 			props.setProperty("adresa", adresa);
@@ -39,6 +77,12 @@ public class KonekcijaPodaci {
 		}
 	}
 
+	/**
+	 * Postavlja novi port na kom server ceka klijenta
+	 * 
+	 * @param port serverskog soketa
+	 * @throws Exception ako dodje do greske pri upisu porta u fajl
+	 */
 	public void postaviPort(String port) {
 		try {
 			props.setProperty("port", port);
