40f8800cfc7e55da3e616ad58e00e41af3ffaf09
iva28 <iva.hurt574@gmail.com>
2023-06-03 01:10:58 +0200

    Dodat javadoc u klasu Main

--- OrdinacijaKlijent/src/main/java/rs/ac/bg/fon/ai/nprog/OrdinacijaKlijent/main/Main.java
+++ OrdinacijaKlijent/src/main/java/rs/ac/bg/fon/ai/nprog/OrdinacijaKlijent/main/Main.java
@@ -3,10 +3,16 @@ package rs.ac.bg.fon.ai.nprog.OrdinacijaKlijent.main;
 import rs.ac.bg.fon.ai.nprog.OrdinacijaKlijent.form.koordinator.Koordinator;
 
 /**
+ * Glavna klasa koja sadrzi metodu main
  *
- * @author Iva
+ * @author Iva Stanisic
  */
 public class Main {
+	/**
+	 * Pokrece aplikaciju otvaranjem forme za prijavu preko Koordinatora
+	 * 
+	 * @param args Argumenti komandne linije
+	 */
     public static void main(String[] args) {
        Koordinator.vratiInstancu().otvoriLoginFormu();
     }
