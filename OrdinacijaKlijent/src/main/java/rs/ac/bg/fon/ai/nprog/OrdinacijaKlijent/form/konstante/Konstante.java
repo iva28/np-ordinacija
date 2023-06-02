58940d921315199780885d582ff5ce0266547e2c
iva28 <iva.hurt574@gmail.com>
2023-06-03 01:09:35 +0200

    Dodat javadoc u klasu Konstante

--- OrdinacijaKlijent/src/main/java/rs/ac/bg/fon/ai/nprog/OrdinacijaKlijent/form/konstante/Konstante.java
+++ OrdinacijaKlijent/src/main/java/rs/ac/bg/fon/ai/nprog/OrdinacijaKlijent/form/konstante/Konstante.java
@@ -1,15 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package rs.ac.bg.fon.ai.nprog.OrdinacijaKlijent.form.konstante;
 
 /**
- *
- * @author Iva
+ * Sadrzi konstante koje predstavljaju razlicite uloge u nutrcionistickoj ordinaciji
+ *  
+ * @author Iva Stanisic
  */
 public class Konstante {
+	/**
+	 * Konstanta koja predstavlja ulogu nutricioniste
+	 */
     public static final String NUTRICIONISTA = "NUTRICIONISTA";
+    /**
+     * Konstanta koja predstavlja ulogu pacijente
+     */
     public static final String PACIJENT = "PACIJENT";
+    /**
+     * Konstanta koja prestavlja plan ishrane koji se izradjuje u ordinaciji
+     */
     public static final String PLAN_ISHRANE = "PLAN_ISHRANE";
 }
