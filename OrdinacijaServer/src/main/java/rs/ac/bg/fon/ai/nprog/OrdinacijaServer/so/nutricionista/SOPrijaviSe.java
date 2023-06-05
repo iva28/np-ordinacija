4b831fa7c3ff676bf05ab1259bed921d95c6dc23
iva28 <iva.hurt574@gmail.com>
2023-06-05 02:31:53 +0200

    Dodat javadoc u SO koje rade sa domenskom klasom Nutricionista

--- OrdinacijaServer/src/main/java/rs/ac/bg/fon/ai/nprog/OrdinacijaServer/so/nutricionista/SOPrijaviSe.java
+++ OrdinacijaServer/src/main/java/rs/ac/bg/fon/ai/nprog/OrdinacijaServer/so/nutricionista/SOPrijaviSe.java
@@ -5,34 +5,46 @@ import rs.ac.bg.fon.ai.nprog.OrdinacijaZajednicki.domen.Nutricionista;
 import rs.ac.bg.fon.ai.nprog.OrdinacijaZajednicki.domen.OpstaDomenskaKlasa;
 
 /**
+ * Predstavlja specificnu operaciju za prijavu nutricioniste
+ * 
+ * Nasledjuje apstraktnu klasu OpstaSO i implementira apstraktne metode iz te
+ * klase. Ova klasa definise logiku za validaciju i izvrsavanje operacije
+ * prijave nutricioniste.
  *
- * @author Iva
+ * @author Iva Stanisic
  */
-public class SOPrijaviSe extends OpstaSO{
-    
-    private Nutricionista nutricionista;
+public class SOPrijaviSe extends OpstaSO {
+	/**
+	 * Nutricionista koji se prijavljuje
+	 */
+	private Nutricionista nutricionista;
 
-    public Nutricionista vratiNutricionistu() {
-        return nutricionista;
-    }
+	/**
+	 * Vraca prijavljenog nutricionistu
+	 * 
+	 * @return nutricionista
+	 */
+	public Nutricionista vratiNutricionistu() {
+		return nutricionista;
+	}
 
-    public void setNutricionista(Nutricionista nutricionista) {
-        this.nutricionista = nutricionista;
-    }
-    
-    
-    @Override
-    protected void izvrsiSpecificnuOperaciju(OpstaDomenskaKlasa obj) throws Exception {
-        nutricionista  =  (Nutricionista) db.vratiJednog(obj);
-    }
+	/**
+	 * Postavlja prijavljenog nutricionistu
+	 * @param nutricionista
+	 */
+	public void setNutricionista(Nutricionista nutricionista) {
+		this.nutricionista = nutricionista;
+	}
 
-    @Override
-    protected void validiraj(OpstaDomenskaKlasa obj) throws Exception {
-        if(obj == null || !(obj instanceof Nutricionista))
-            throw new Exception("Mora biti Nutricionista!\n");
-    }
+	@Override
+	protected void izvrsiSpecificnuOperaciju(OpstaDomenskaKlasa obj) throws Exception {
+		nutricionista = (Nutricionista) db.vratiJednog(obj);
+	}
 
-    
-   
-    
+	@Override
+	protected void validiraj(OpstaDomenskaKlasa obj) throws Exception {
+		if (obj == null || !(obj instanceof Nutricionista))
+			throw new Exception("Mora biti Nutricionista!\n");
+	}
+
 }
--- OrdinacijaServer/src/main/java/rs/ac/bg/fon/ai/nprog/OrdinacijaServer/so/nutricionista/SOUcitajListuNutricionista.java
+++ OrdinacijaServer/src/main/java/rs/ac/bg/fon/ai/nprog/OrdinacijaServer/so/nutricionista/SOUcitajListuNutricionista.java
@@ -5,28 +5,41 @@ import java.util.List;
 import rs.ac.bg.fon.ai.nprog.OrdinacijaServer.so.OpstaSO;
 import rs.ac.bg.fon.ai.nprog.OrdinacijaZajednicki.domen.Nutricionista;
 import rs.ac.bg.fon.ai.nprog.OrdinacijaZajednicki.domen.OpstaDomenskaKlasa;
+
 /**
+ * Predstavlja specificnu operaciju za ucitavanje svih nutricionista
+ * 
+ * Nasledjuje apstraktnu klasu OpstaSO i implementira apstraktne metode iz te
+ * klase. Ova klasa definise logiku za validaciju i izvrsavanje operacije
+ * ucitavanja svih nutricionista.
  *
- * @author Iva
+ * @author Iva Stanisic
  */
 public class SOUcitajListuNutricionista extends OpstaSO {
+	/**
+	 * Predstavlja listu nutricionista
+	 */
+	private List<OpstaDomenskaKlasa> lista;
 
-    private List<OpstaDomenskaKlasa> lista;
+	/**
+	 * Vraca listu svih nutricionista
+	 * 
+	 * @return lista nutricionista
+	 */
+	public List<OpstaDomenskaKlasa> getLista() {
+		return lista;
+	}
 
-    public List<OpstaDomenskaKlasa> getLista() {
-        return lista;
-    }
-    
-    @Override
-    protected void izvrsiSpecificnuOperaciju(OpstaDomenskaKlasa obj) throws Exception {
-        lista = db.svi(obj);
-    }
+	@Override
+	protected void izvrsiSpecificnuOperaciju(OpstaDomenskaKlasa obj) throws Exception {
+		lista = db.svi(obj);
+	}
 
-    @Override
-    protected void validiraj(OpstaDomenskaKlasa obj) throws Exception {
-        if (obj == null || !(obj instanceof Nutricionista)) {
-            throw new Exception("Mora biti Nutricionista!\n");
-        }
-    }
+	@Override
+	protected void validiraj(OpstaDomenskaKlasa obj) throws Exception {
+		if (obj == null || !(obj instanceof Nutricionista)) {
+			throw new Exception("Mora biti Nutricionista!\n");
+		}
+	}
 
 }
