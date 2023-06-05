a4ec802d729b888881f3c9063af1aae09ce920a8
iva28 <iva.hurt574@gmail.com>
2023-06-05 02:32:05 +0200

    Dodat javadoc u klasu Main

--- OrdinacijaServer/src/main/java/rs/ac/bg/fon/ai/nprog/OrdinacijaServer/main/Main.java
+++ OrdinacijaServer/src/main/java/rs/ac/bg/fon/ai/nprog/OrdinacijaServer/main/Main.java
@@ -3,11 +3,18 @@ package rs.ac.bg.fon.ai.nprog.OrdinacijaServer.main;
 import rs.ac.bg.fon.ai.nprog.OrdinacijaServer.form.koordinator.*;
 
 /**
- *
- * @author Iva
+ * Sluzi kao ulazna tacka za aplikaciju
+ * 
+ * @author Iva Stanisic
  */
 public class Main {
-
+	/**
+     * Metoda main je pocetna tacka aplikacije
+     * 
+     * Poziva se metoda otvoriServerFormu() klase KoordinatorS kako bi se otvorila forma servera.
+     *
+     * @param args Argumenti komandne linije koji se prosledjuju aplikaciji 
+     */
 	public static void main(String[] args) {
 		 KoordinatorS.vratiInstancu().otvoriServerFormu();
 	}
