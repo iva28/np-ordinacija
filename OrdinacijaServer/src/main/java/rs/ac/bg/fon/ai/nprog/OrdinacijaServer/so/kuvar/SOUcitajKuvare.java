51f0521386990b81cd338455b3d2dd20abb3b935
iva28 <iva.hurt574@gmail.com>
2023-06-05 02:30:53 +0200

    Dodat javadoc u SO SOUcitajKuvare

--- OrdinacijaServer/src/main/java/rs/ac/bg/fon/ai/nprog/OrdinacijaServer/so/kuvar/SOUcitajKuvare.java
+++ OrdinacijaServer/src/main/java/rs/ac/bg/fon/ai/nprog/OrdinacijaServer/so/kuvar/SOUcitajKuvare.java
@@ -7,26 +7,38 @@ import rs.ac.bg.fon.ai.nprog.OrdinacijaZajednicki.domen.Kuvar;
 import rs.ac.bg.fon.ai.nprog.OrdinacijaZajednicki.domen.OpstaDomenskaKlasa;
 
 /**
+ * Predstavlja specificnu operaciju za ucitavanje svih kuvara
+ * 
+ * Nasledjuje apstraktnu klasu OpstaSO i implementira apstraktne metode iz te
+ * klase. Ova klasa definise logiku za validaciju i izvrsavanje operacije
+ * ucitavanja svih kuvara.
  *
- * @author Iva
+ * @author Iva Stanisic
  */
-public class SOUcitajKuvare extends OpstaSO{
-    
-      private List<OpstaDomenskaKlasa> lista;
+public class SOUcitajKuvare extends OpstaSO {
+	/**
+	 * Predstavlja listu kuvara
+	 */
+	private List<OpstaDomenskaKlasa> lista;
 
-    public List<OpstaDomenskaKlasa> vratiListu() {
-        return lista;
-    }
+	/**
+	 * Vraca listu svih kuvara
+	 * 
+	 * @return lista kuvara
+	 */
+	public List<OpstaDomenskaKlasa> vratiListu() {
+		return lista;
+	}
 
-    @Override
-    protected void izvrsiSpecificnuOperaciju(OpstaDomenskaKlasa obj) throws Exception {
-        lista = db.svi(obj);
-    }
+	@Override
+	protected void izvrsiSpecificnuOperaciju(OpstaDomenskaKlasa obj) throws Exception {
+		lista = db.svi(obj);
+	}
 
-    @Override
-    protected void validiraj(OpstaDomenskaKlasa obj) throws Exception {
-        if (!(obj instanceof Kuvar))
-            throw new Exception("Mora biti Kuvar!\n");
-    }
-    
+	@Override
+	protected void validiraj(OpstaDomenskaKlasa obj) throws Exception {
+		if (!(obj instanceof Kuvar))
+			throw new Exception("Mora biti Kuvar!\n");
+	}
+
 }
