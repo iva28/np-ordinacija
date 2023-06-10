3cc3c44f84085be0c7df27414a738da6eebb2edd
iva28 <iva.hurt574@gmail.com>
2023-06-10 11:46:50 +0200

    Dodat javadoc za klijent niti i kontrolera koji komunicira sa klijentima app

--- OrdinacijaServer/src/main/java/rs/ac/bg/fon/ai/nprog/OrdinacijaServer/kontroler/Kontroler.java
+++ OrdinacijaServer/src/main/java/rs/ac/bg/fon/ai/nprog/OrdinacijaServer/kontroler/Kontroler.java
@@ -10,18 +10,38 @@ import rs.ac.bg.fon.ai.nprog.OrdinacijaServer.so.kuvar.*;
 import rs.ac.bg.fon.ai.nprog.OrdinacijaServer.so.jela.*;
 import rs.ac.bg.fon.ai.nprog.OrdinacijaZajednicki.domen.*;
 
-/*
+/**
+ * Predstavlja kontrolera koji upravlja sistemskim operacijama
  *
- * @author Iva
+ * Implementira singleton obrazac
+ * 
+ * @author Iva Stanisic
  */
 public class Kontroler {
-
+	/**
+	 * Privatno staticko polje koje cuva jedinstvenu instancu klase Kontroler
+	 */
 	private static Kontroler instanca;
+	/**
+	 * Polje koje cuva referencu na objekat OpstaSO koji se koristi za izvrsavanje
+	 * operacija
+	 */
 	private OpstaSO operacija;
 
+	/**
+	 * Privatni konstruktor klase Kontroler koji onemogucava direktno instanciranje
+	 * objekata klase
+	 */
 	private Kontroler() {
 	}
 
+	/**
+	 * Vraca instancu klase Kontroler
+	 * 
+	 * Ako instanca ne postoji, kreira je i vraca
+	 *
+	 * @return Instanca klase Kontroler
+	 */
 	public static Kontroler vratiInstancu() {
 		if (instanca == null) {
 			instanca = new Kontroler();
@@ -29,114 +49,240 @@ public class Kontroler {
 		return instanca;
 	}
 
+	/**
+	 * Prijavljuje nutricionistu na sistem
+	 *
+	 * @param n Nutricionista koji se prijavljuje
+	 * @return Prijavljeni nutricionista
+	 * @throws Exception ako se dogodi greska prilikom izvrsavanja operacije
+	 */
 	public Nutricionista prijaviSe(Nutricionista n) throws Exception {
 		operacija = new SOPrijaviSe();
 		operacija.izvrsiOperaciju(n);
 		return ((SOPrijaviSe) operacija).vratiNutricionistu();
 	}
 
+	/**
+	 * Ucitava listu pacijenata
+	 * 
+	 * @return Lista pacijenata
+	 * @throws Exception ako se dogodi greska prilikom izvrsavanja operacije
+	 */
 	public List<OpstaDomenskaKlasa> ucitajListuPacijenata() throws Exception {
 		operacija = new SOUcitajListuPacijenata();
 		operacija.izvrsiOperaciju(new Pacijent());
 		return ((SOUcitajListuPacijenata) operacija).getLista();
 	}
 
+	/**
+	 * Kreira pacijenta
+	 * 
+	 * @param pacijent Pacijent koji se kreira
+	 * @return Vrednost boolean koja oznacava uspeh kreiranja pacijenta
+	 * @throws Exception ako se dogodi greska prilikom izvrsavanja operacije
+	 */
 	public boolean kreirajPacijenta(Pacijent pacijent) throws Exception {
 		operacija = new SOKreirajPacijenta();
 		operacija.izvrsiOperaciju(pacijent);
 		return ((SOKreirajPacijenta) operacija).isUspeh();
 	}
 
+	/**
+	 * Brise pacijenta
+	 * 
+	 * @param pacijent Pacijent koji se brise
+	 * @return Vrednost boolean koja oznacava uspeh brisanja pacijenta
+	 * @throws Exception ako se dogodi greska prilikom izvrsavanja operacije
+	 */
 	public boolean obrisiPacijenta(Pacijent pacijent) throws Exception {
 		operacija = new SOObrisiPacijenta();
 		operacija.izvrsiOperaciju(pacijent);
 		return ((SOObrisiPacijenta) operacija).isUspeh();
 	}
 
+	/**
+	 * Menja pacijenta
+	 *
+	 * @param pacijent Pacijent koji se menja
+	 * @return Vrednost boolean koja oznacava uspeh izmene pacijenta
+	 * @throws Exception ako se dogodi greska prilikom izvrsavanja operacije
+	 */
 	public boolean izmeniPacijenta(Pacijent pacijent) throws Exception {
 		operacija = new SOIzmeniPacijenta();
 		operacija.izvrsiOperaciju(pacijent);
 		return ((SOIzmeniPacijenta) operacija).isUspeh();
 	}
 
+	/**
+	 * Pretrazuje pacijente
+	 * 
+	 * @param pacijent Pacijent na osnovu kojeg se vrsi pretraga
+	 * @return lista nadjenih pacijenata koji odgovaraju kriterijumu pretrage
+	 * @throws Exception ako se dogodi greska prilikom izvrsavanja operacije
+	 */
 	public List<OpstaDomenskaKlasa> nadjiPacijente(Pacijent pacijent) throws Exception {
 		operacija = new SONadjiPacijente();
 		operacija.izvrsiOperaciju(pacijent);
 		return ((SONadjiPacijente) operacija).vratiListu();
 	}
 
+	/**
+	 * Ucitava listu tipova jela
+	 * 
+	 * @return Lista tipova jela
+	 * @throws Exception ako se dogodi greska prilikom izvrsavanja operacije
+	 */
 	public List<OpstaDomenskaKlasa> ucitajTipoveJela() throws Exception {
 		operacija = new SOUcitajTipoveJela();
 		operacija.izvrsiOperaciju(new TipJela());
 		return ((SOUcitajTipoveJela) operacija).vratiListu();
 	}
 
+	/**
+	 * Ucitava listu kuvara
+	 * 
+	 * @return Lista kuvara
+	 * @throws Exception ako se dogodi greska prilikom izvrsavanja operacije
+	 */
 	public List<OpstaDomenskaKlasa> ucitajKuvare() throws Exception {
 		operacija = new SOUcitajKuvare();
 		operacija.izvrsiOperaciju(new Kuvar());
 		return ((SOUcitajKuvare) operacija).vratiListu();
 	}
 
+	/**
+	 * Kreira jelo
+	 * 
+	 * @param jelo Jelo koje se kreira
+	 * @return Vrednost boolean koja oznacava uspeh kreiranja jela
+	 * @throws Exception ako se dogodi greska prilikom izvrsavanja operacije
+	 */
 	public boolean kreirajJelo(Jelo jelo) throws Exception {
 		operacija = new SOKreirajJelo();
 		operacija.izvrsiOperaciju(jelo);
 		return ((SOKreirajJelo) operacija).isUspeh();
 	}
 
+	/**
+	 * Ucitava listu jela
+	 * 
+	 * @return Lista jela
+	 * @throws Exception ako se dogodi greska prilikom izvrsavanja operacije
+	 */
 	public List<OpstaDomenskaKlasa> ucitajListuJela() throws Exception {
 		operacija = new SOUcitajListuJela();
 		operacija.izvrsiOperaciju(new Jelo());
 		return ((SOUcitajListuJela) operacija).vratiListu();
 	}
 
+	/**
+	 * Pretrazuje jela
+	 * 
+	 * @param jelo Jelo na osnovu kojeg se pretrazuje
+	 * @return Lista jela koje odgovaraju kriterijumu pretrage
+	 * @throws Exception ako se dogodi greska prilikom izvrsavanja operacije
+	 */
 	public List<OpstaDomenskaKlasa> nadjiJela(Jelo jelo) throws Exception {
 		operacija = new SONadjiJela();
 		operacija.izvrsiOperaciju(jelo);
 		return ((SONadjiJela) operacija).vratiListu();
 	}
 
+	/**
+	 * Kreira plan ishrane
+	 *
+	 * @param plan Plan ishrane koji se kreira
+	 * @return Vrednost boolean koja oznacava uspeh kreiranja plana ishrane
+	 * @throws Exception ako se dogodi greska prilikom izvrsavanja operacije
+	 */
 	public boolean kreirajPlanIshrane(PlanIshrane plan) throws Exception {
 		operacija = new SOKreirajPlanIshrane();
 		operacija.izvrsiOperaciju(plan);
 		return ((SOKreirajPlanIshrane) operacija).isUspeh();
 	}
 
+	/**
+	 * Ucitava listu planova ishrane
+	 * 
+	 * @return Lista planova ishrane
+	 * @throws Exception ako se dogodi greska prilikom izvrsavanja operacije
+	 */
 	public List<OpstaDomenskaKlasa> ucitajListuPlanova() throws Exception {
 		operacija = new SOUcitajListuPlanova();
 		operacija.izvrsiOperaciju(new PlanIshrane());
 		return ((SOUcitajListuPlanova) operacija).vratiListu();
 	}
 
+	/**
+	 * Pretrazuje planove ishrana
+	 * 
+	 * @param plan Plan ishrane na osnovu kojeg se vrsi pretraga
+	 * @return Lista planova ishrana koji odgovaraju kriterijumu pretrage
+	 * @throws Exception ako se dogodi greska prilikom izvrsavanja operacije
+	 */
 	public List<OpstaDomenskaKlasa> nadjiPlanove(PlanIshrane plan) throws Exception {
 		operacija = new SONadjiPlanoveIshrana();
 		operacija.izvrsiOperaciju(plan);
 		return ((SONadjiPlanoveIshrana) operacija).vratiListu();
 	}
 
+	/**
+	 * Brise plan ishrane
+	 * 
+	 * @param plan Plan ishrane koji se brise
+	 * @return Vrednost boolean koja oznacava uspeh brisanja plana ishrane
+	 * @throws Exception ako se dogodi greska prilikom izvrsavanja operacije
+	 */
 	public boolean obrisiPlan(PlanIshrane plan) throws Exception {
 		operacija = new SOObrisiPlanIshrane();
 		operacija.izvrsiOperaciju(plan);
 		return ((SOObrisiPlanIshrane) operacija).isUspeh();
 	}
 
+	/**
+	 * Menja plan ishrane
+	 * 
+	 * @param plan Plan ishrane koji se menja
+	 * @return Vrednost boolean koja oznacava uspeh izmene plana ishrane
+	 * @throws Exception ako se dogodi greska prilikom izvrsavanja operacije
+	 */
 	public boolean izmeniPlanIshrane(PlanIshrane plan) throws Exception {
 		operacija = new SOIzmeniPlanIshrane();
 		operacija.izvrsiOperaciju(plan);
 		return ((SOIzmeniPlanIshrane) operacija).isUspeh();
 	}
 
+	/**
+	 * Ucitava listu nutricionista
+	 *
+	 * @return Lista nutricionista
+	 * @throws Exception ako se dogodi greska prilikom izvrsavanja operacije
+	 */
 	public List<OpstaDomenskaKlasa> ucitajListuNutricionista() throws Exception {
 		operacija = new SOUcitajListuNutricionista();
 		operacija.izvrsiOperaciju(new Nutricionista());
 		return ((SOUcitajListuNutricionista) operacija).getLista();
 	}
 
+	/**
+	 * Ucitava pacijenta
+	 * 
+	 * @param pacijent Pacijent ciji se podaci ucitavaju
+	 * @return Objekat klase Pacijent
+	 * @throws Exception ako se dogodi greska prilikom izvrsavanja operacije
+	 */
 	public Pacijent ucitajPacijenta(Pacijent pacijent) throws Exception {
 		operacija = new SOUcitajPacijenta();
 		operacija.izvrsiOperaciju(pacijent);
 		return ((SOUcitajPacijenta) operacija).getPacijent();
 	}
-
+	/**
+	 * Ucitava plan ishrane
+	 * 
+	 * @param plan Plan ishrane ciji se podaci ucitavaju
+	 * @return Objekat klase PlanIshrane koji predstavlja ucitane podatke
+	 * @throws Exception ako se dogodi greska prilikom izvrsavanja operacije
+	 */
 	public PlanIshrane ucitajPlanIshrane(PlanIshrane plan) throws Exception {
 		operacija = new SOUcitajPlanIshrane();
 		operacija.izvrsiOperaciju(plan);
--- OrdinacijaServer/src/main/java/rs/ac/bg/fon/ai/nprog/OrdinacijaServer/niti/KlijentNit.java
+++ OrdinacijaServer/src/main/java/rs/ac/bg/fon/ai/nprog/OrdinacijaServer/niti/KlijentNit.java
@@ -11,157 +11,195 @@ import rs.ac.bg.fon.ai.nprog.OrdinacijaZajednicki.komunikacija.Sender;
 import rs.ac.bg.fon.ai.nprog.OrdinacijaZajednicki.transfer.*;
 
 /**
- *
- * @author Iva
+ * Nit koja predstavlja komunikaciju sa jednim klijentom
+ * 
+ * Svaki nutricionista pri prijavljivanju na sistem dobija svoju nit
+ * 
+ * @author Iva Stanisic
  */
 public class KlijentNit extends Thread {
 
-    private final Socket soket;
-    private final Receiver receiver;
-    private final Sender sender;
+	/**
+	 * Predstavlja soket koji se koristi za komunikaciju sa klijentom
+	 */
+	private final Socket soket;
+	/**
+	 * Predstavlja objekat Receiver koji se koristi za primanje poruka klijenta
+	 */
+	private final Receiver receiver;
+	/**
+	 * Predstavlja objekat Sender koji se koristi za slanje poruka klijentu
+	 */
+	private final Sender sender;
 
-    private Nutricionista administrator;
+	/**
+	 * Predstavlja administratora sistema
+	 */
+	private Nutricionista administrator;
 
-    public Nutricionista getAdministrator() {
-        return administrator;
-    }
+	/**
+	 * Vraca nutricionistu koji predstavlja administratora sistema
+	 * 
+	 * @return administrator nutricionista koji predstavlja administratora
+	 */
+	public Nutricionista getAdministrator() {
+		return administrator;
+	}
 
-    public KlijentNit(Socket soket) {
-        this.soket = soket;
-        receiver = new Receiver(soket);
-        sender = new Sender(soket);
-    }
+	/**
+	 * Konstruktor za inicijalizaciju klase KlijentNit
+	 * 
+	 * @param soket Soket koji se koristi za komunikaciju sa klijentom
+	 */
+	public KlijentNit(Socket soket) {
+		this.soket = soket;
+		receiver = new Receiver(soket);
+		sender = new Sender(soket);
+	}
 
-    public Socket getSoket() {
-        return soket;
-    }
+	/**
+	 * Vraca soket koji se koristi za komunikaciju sa klijentom
+	 * 
+	 * @return Objekat Socket koji predstavlja soket za komunikaciju
+	 */
+	public Socket getSoket() {
+		return soket;
+	}
 
-    @Override
-    public void run() {
-        while (!soket.isClosed()) {
-            try {
-                Zahtev zahtev = (Zahtev) receiver.primiZahtev();
-                Odgovor odgovor = new Odgovor();
-                try {
-                    switch (zahtev.getOperacija()) {
-                        case PRIJAVI_SE:
-                            Nutricionista nutricionistaPrijava = (Nutricionista) zahtev.getObjekat();
-                            nutricionistaPrijava = Kontroler.vratiInstancu().prijaviSe(nutricionistaPrijava);
-                            if (nutricionistaPrijava != null) {
-                                this.administrator = nutricionistaPrijava;
-                                boolean aktivan = KoordinatorS.vratiInstancu().vecJeUlogovan(nutricionistaPrijava);
-                                if (aktivan) {
-                                    odgovor.setRezultat(new Nutricionista(-1L));
-                                } else {
-                                    KoordinatorS.vratiInstancu().getAktivniKorisnici().add(this);
-                                    KoordinatorS.vratiInstancu().getKontrolerPokreniServer().dodajNutricionistu(nutricionistaPrijava);
-                                    odgovor.setRezultat(nutricionistaPrijava);
-                                }
-                            } else {
-                                odgovor.setRezultat(null);
-                            }
-                            break;
-                        case ODJAVI_SE:
-                            Nutricionista nutricionistaOdjava = (Nutricionista) zahtev.getObjekat();
-                            boolean rezultatOdjave = KoordinatorS.vratiInstancu().odjaviSe(nutricionistaOdjava);
-                            if (rezultatOdjave) {
-                                KoordinatorS.vratiInstancu().getAktivniKorisnici().remove(this);
-                            }
-                            odgovor.setRezultat(rezultatOdjave);
-                            break;
-                        case VRATI_NUTRICIONISTE:
-                            List<OpstaDomenskaKlasa> listaSviNutricionisti = Kontroler.vratiInstancu().ucitajListuNutricionista();
-                            odgovor.setRezultat(listaSviNutricionisti);
-                            break;
-                        case SVI_PACIJENTI:
-                            List<OpstaDomenskaKlasa> listaSviPacijenti = Kontroler.vratiInstancu().ucitajListuPacijenata();
-                            odgovor.setRezultat(listaSviPacijenti);
-                            break;
-                        case KREIRAJ_PACIJENTA:
-                            Pacijent pacijentNovi = (Pacijent) zahtev.getObjekat();
-                            boolean rezultatKreiranjaPacijenta = Kontroler.vratiInstancu().kreirajPacijenta(pacijentNovi);
-                            odgovor.setRezultat(rezultatKreiranjaPacijenta);
-                            break;
-                        case OBRISI_PACIJENTA:
-                            Pacijent pacijentObrisi = (Pacijent) zahtev.getObjekat();
-                            boolean rezultatBrisanjaPacijenta = Kontroler.vratiInstancu().obrisiPacijenta(pacijentObrisi);
-                            odgovor.setRezultat(rezultatBrisanjaPacijenta);
-                            break;
-                        case IZMENI_PACIJENTA:
-                            Pacijent pacijentIzmena = (Pacijent) zahtev.getObjekat();
-                            boolean rezultatIzmenePacijenta = Kontroler.vratiInstancu().izmeniPacijenta(pacijentIzmena);
-                            odgovor.setRezultat(rezultatIzmenePacijenta);
-                            break;
-                        case NADJI_PACIJENTE:
-                            Pacijent pacijentUslov = (Pacijent) zahtev.getObjekat();
-                            List<OpstaDomenskaKlasa> listaNadjenihPacijenata = Kontroler.vratiInstancu().nadjiPacijente(pacijentUslov);
-                            odgovor.setRezultat(listaNadjenihPacijenata);
-                            break;
-                        case UCITAJ_PACIJENTA:
-                            Pacijent pacijentUcitaj = (Pacijent) zahtev.getObjekat();
-                            pacijentUcitaj = Kontroler.vratiInstancu().ucitajPacijenta(pacijentUcitaj);
-                            odgovor.setRezultat(pacijentUcitaj);
-                            break;
-                        case VRATI_TIP_JELA:
-                            List<OpstaDomenskaKlasa> listaSviTipoviJela = Kontroler.vratiInstancu().ucitajTipoveJela();
-                            odgovor.setRezultat(listaSviTipoviJela);
-                            break;
-                        case VRATI_KUVARE:
-                            List<OpstaDomenskaKlasa> listaSviKuvari = Kontroler.vratiInstancu().ucitajKuvare();
-                            odgovor.setRezultat(listaSviKuvari);
-                            break;
-                        case KREIRAJ_JELO:
-                            Jelo jeloKreiraj = (Jelo) zahtev.getObjekat();
-                            boolean rezultatKreirajJelo = Kontroler.vratiInstancu().kreirajJelo(jeloKreiraj);
-                            odgovor.setRezultat(rezultatKreirajJelo);
-                            break;
-                        case VRATI_JELA:
-                            List<OpstaDomenskaKlasa> listaSvaJela = Kontroler.vratiInstancu().ucitajListuJela();
-                            odgovor.setRezultat(listaSvaJela);
-                            break;
-                        case NADJI_JELA:
-                            Jelo jeloUslov = (Jelo) zahtev.getObjekat();
-                            List<OpstaDomenskaKlasa> listaNadjiJela = Kontroler.vratiInstancu().nadjiJela(jeloUslov);
-                            odgovor.setRezultat(listaNadjiJela);
-                            break;
-                        case KREIRAJ_PLAN_ISHRANE:
-                            PlanIshrane planKreiraj = (PlanIshrane) zahtev.getObjekat();
-                            boolean rezultatKreirajPlan = Kontroler.vratiInstancu().kreirajPlanIshrane(planKreiraj);
-                            odgovor.setRezultat(rezultatKreirajPlan);
-                            break;
-                        case VRATI_PLANOVE:
-                            List<OpstaDomenskaKlasa> listaSviPlanovi = Kontroler.vratiInstancu().ucitajListuPlanova();
-                            odgovor.setRezultat(listaSviPlanovi);
-                            break;
-                        case NADJI_PLAN_ISHRANE:
-                            PlanIshrane planUslov = (PlanIshrane) zahtev.getObjekat();
-                            List<OpstaDomenskaKlasa> listaNadjiPlanove = Kontroler.vratiInstancu().nadjiPlanove(planUslov);
-                            odgovor.setRezultat(listaNadjiPlanove);
-                            break;
-                        case UCITAJ_PLAN_ISHRANE:
-                            PlanIshrane planUcitaj = (PlanIshrane) zahtev.getObjekat();
-                            planUcitaj = Kontroler.vratiInstancu().ucitajPlanIshrane(planUcitaj);
-                            odgovor.setRezultat(planUcitaj);
-                            break;
-                        case OBRISI_PLAN_ISHRANE:
-                            PlanIshrane planObrisi = (PlanIshrane) zahtev.getObjekat();
-                            boolean rezultatObrisiPlan = Kontroler.vratiInstancu().obrisiPlan(planObrisi);
-                            odgovor.setRezultat(rezultatObrisiPlan);
-                            break;
-                        case IZMENI_PLAN_ISHRANE:
-                            PlanIshrane planIzmena = (PlanIshrane) zahtev.getObjekat();
-                            boolean rezultatIzmenaPlana = Kontroler.vratiInstancu().izmeniPlanIshrane(planIzmena);
-                            odgovor.setRezultat(rezultatIzmenaPlana);
-                            break;
-                    }
-                } catch (Exception ex) {
-                    odgovor.setException(ex);
-                }
-                sender.posaljiZahtev(odgovor);
-            } catch (Exception e) {
-            } 
-        }
-    }
+	/**
+	 * Pokrece izvrsavanje klijentske niti
+	 * 
+	 * Tokom izvrsavanja niti, prihvata zahteve od klijenta i vrsi odgovarajuce operacije. 
+	 * Ukoliko se soket zatvori, nit se zaustavlja.
+	 */
+	@Override
+	public void run() {
+		while (!soket.isClosed()) {
+			try {
+				Zahtev zahtev = (Zahtev) receiver.primiZahtev();
+				Odgovor odgovor = new Odgovor();
+				try {
+					switch (zahtev.getOperacija()) {
+					case PRIJAVI_SE:
+						Nutricionista nutricionistaPrijava = (Nutricionista) zahtev.getObjekat();
+						nutricionistaPrijava = Kontroler.vratiInstancu().prijaviSe(nutricionistaPrijava);
+						if (nutricionistaPrijava != null) {
+							this.administrator = nutricionistaPrijava;
+							boolean aktivan = KoordinatorS.vratiInstancu().vecJeUlogovan(nutricionistaPrijava);
+							if (aktivan) {
+								odgovor.setRezultat(new Nutricionista(-1L));
+							} else {
+								KoordinatorS.vratiInstancu().getAktivniKorisnici().add(this);
+								KoordinatorS.vratiInstancu().getKontrolerPokreniServer()
+										.dodajNutricionistu(nutricionistaPrijava);
+								odgovor.setRezultat(nutricionistaPrijava);
+							}
+						} else {
+							odgovor.setRezultat(null);
+						}
+						break;
+					case ODJAVI_SE:
+						Nutricionista nutricionistaOdjava = (Nutricionista) zahtev.getObjekat();
+						boolean rezultatOdjave = KoordinatorS.vratiInstancu().odjaviSe(nutricionistaOdjava);
+						if (rezultatOdjave) {
+							KoordinatorS.vratiInstancu().getAktivniKorisnici().remove(this);
+						}
+						odgovor.setRezultat(rezultatOdjave);
+						break;
+					case VRATI_NUTRICIONISTE:
+						List<OpstaDomenskaKlasa> listaSviNutricionisti = Kontroler.vratiInstancu()
+								.ucitajListuNutricionista();
+						odgovor.setRezultat(listaSviNutricionisti);
+						break;
+					case SVI_PACIJENTI:
+						List<OpstaDomenskaKlasa> listaSviPacijenti = Kontroler.vratiInstancu().ucitajListuPacijenata();
+						odgovor.setRezultat(listaSviPacijenti);
+						break;
+					case KREIRAJ_PACIJENTA:
+						Pacijent pacijentNovi = (Pacijent) zahtev.getObjekat();
+						boolean rezultatKreiranjaPacijenta = Kontroler.vratiInstancu().kreirajPacijenta(pacijentNovi);
+						odgovor.setRezultat(rezultatKreiranjaPacijenta);
+						break;
+					case OBRISI_PACIJENTA:
+						Pacijent pacijentObrisi = (Pacijent) zahtev.getObjekat();
+						boolean rezultatBrisanjaPacijenta = Kontroler.vratiInstancu().obrisiPacijenta(pacijentObrisi);
+						odgovor.setRezultat(rezultatBrisanjaPacijenta);
+						break;
+					case IZMENI_PACIJENTA:
+						Pacijent pacijentIzmena = (Pacijent) zahtev.getObjekat();
+						boolean rezultatIzmenePacijenta = Kontroler.vratiInstancu().izmeniPacijenta(pacijentIzmena);
+						odgovor.setRezultat(rezultatIzmenePacijenta);
+						break;
+					case NADJI_PACIJENTE:
+						Pacijent pacijentUslov = (Pacijent) zahtev.getObjekat();
+						List<OpstaDomenskaKlasa> listaNadjenihPacijenata = Kontroler.vratiInstancu()
+								.nadjiPacijente(pacijentUslov);
+						odgovor.setRezultat(listaNadjenihPacijenata);
+						break;
+					case UCITAJ_PACIJENTA:
+						Pacijent pacijentUcitaj = (Pacijent) zahtev.getObjekat();
+						pacijentUcitaj = Kontroler.vratiInstancu().ucitajPacijenta(pacijentUcitaj);
+						odgovor.setRezultat(pacijentUcitaj);
+						break;
+					case VRATI_TIP_JELA:
+						List<OpstaDomenskaKlasa> listaSviTipoviJela = Kontroler.vratiInstancu().ucitajTipoveJela();
+						odgovor.setRezultat(listaSviTipoviJela);
+						break;
+					case VRATI_KUVARE:
+						List<OpstaDomenskaKlasa> listaSviKuvari = Kontroler.vratiInstancu().ucitajKuvare();
+						odgovor.setRezultat(listaSviKuvari);
+						break;
+					case KREIRAJ_JELO:
+						Jelo jeloKreiraj = (Jelo) zahtev.getObjekat();
+						boolean rezultatKreirajJelo = Kontroler.vratiInstancu().kreirajJelo(jeloKreiraj);
+						odgovor.setRezultat(rezultatKreirajJelo);
+						break;
+					case VRATI_JELA:
+						List<OpstaDomenskaKlasa> listaSvaJela = Kontroler.vratiInstancu().ucitajListuJela();
+						odgovor.setRezultat(listaSvaJela);
+						break;
+					case NADJI_JELA:
+						Jelo jeloUslov = (Jelo) zahtev.getObjekat();
+						List<OpstaDomenskaKlasa> listaNadjiJela = Kontroler.vratiInstancu().nadjiJela(jeloUslov);
+						odgovor.setRezultat(listaNadjiJela);
+						break;
+					case KREIRAJ_PLAN_ISHRANE:
+						PlanIshrane planKreiraj = (PlanIshrane) zahtev.getObjekat();
+						boolean rezultatKreirajPlan = Kontroler.vratiInstancu().kreirajPlanIshrane(planKreiraj);
+						odgovor.setRezultat(rezultatKreirajPlan);
+						break;
+					case VRATI_PLANOVE:
+						List<OpstaDomenskaKlasa> listaSviPlanovi = Kontroler.vratiInstancu().ucitajListuPlanova();
+						odgovor.setRezultat(listaSviPlanovi);
+						break;
+					case NADJI_PLAN_ISHRANE:
+						PlanIshrane planUslov = (PlanIshrane) zahtev.getObjekat();
+						List<OpstaDomenskaKlasa> listaNadjiPlanove = Kontroler.vratiInstancu().nadjiPlanove(planUslov);
+						odgovor.setRezultat(listaNadjiPlanove);
+						break;
+					case UCITAJ_PLAN_ISHRANE:
+						PlanIshrane planUcitaj = (PlanIshrane) zahtev.getObjekat();
+						planUcitaj = Kontroler.vratiInstancu().ucitajPlanIshrane(planUcitaj);
+						odgovor.setRezultat(planUcitaj);
+						break;
+					case OBRISI_PLAN_ISHRANE:
+						PlanIshrane planObrisi = (PlanIshrane) zahtev.getObjekat();
+						boolean rezultatObrisiPlan = Kontroler.vratiInstancu().obrisiPlan(planObrisi);
+						odgovor.setRezultat(rezultatObrisiPlan);
+						break;
+					case IZMENI_PLAN_ISHRANE:
+						PlanIshrane planIzmena = (PlanIshrane) zahtev.getObjekat();
+						boolean rezultatIzmenaPlana = Kontroler.vratiInstancu().izmeniPlanIshrane(planIzmena);
+						odgovor.setRezultat(rezultatIzmenaPlana);
+						break;
+					}
+				} catch (Exception ex) {
+					odgovor.setException(ex);
+				}
+				sender.posaljiZahtev(odgovor);
+			} catch (Exception e) {
+			}
+		}
+	}
 
-    
 }
